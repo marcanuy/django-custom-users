8ae54f76d8e8666673f732f5b3b1f8c43a687276
Marcelo Canina <me@marcanuy.com>
2018-09-21 17:02:42 -0300

    add profile page

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('profile/', TemplateView.as_view(template_name='profile.html'), name='profile'),
 ]
